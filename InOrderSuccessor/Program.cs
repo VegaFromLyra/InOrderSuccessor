3cf933b965ce47bc00226cbf5218842f49964183
VegaFromLyra <asha.ab@gmail.com>
2014-03-15 11:56:05 -0700

    Updated code and added test cases

--- InOrderSuccessor/Program.cs
+++ InOrderSuccessor/Program.cs
@@ -27,15 +27,16 @@ using System.Threading.Tasks;
 
 //if n has a right subtree
 //  result = left most node of right subtree
-//else if n.Data < n.Parent.Data
-//  result = n.Parent
 //else
-//  while (n is a righ child of parent)
+//  while (n is a right child of parent AND n is not null)
 //  {
 //     n = n.parent
 //  }
-//  result = n.Parent;
 
+//   if (n is null)
+//      return null
+//
+//  result = n.Parent;
 //return result; 
 
 namespace InOrderSuccessor
@@ -44,9 +45,64 @@ namespace InOrderSuccessor
     {
         static void Main(string[] args)
         {
+            Node root = new Node(8);
+            root.Left = new Node(4);
+            root.Right = new Node(10);
+            root.Left.Parent = root;
+            root.Right.Parent = root;
+
+            root.Right.Left = new Node(9);
+            root.Right.Right = new Node(14);
+            root.Right.Left.Parent = root.Right;
+            root.Right.Right.Parent = root.Right;
+
+            root.Right.Right.Left = new Node(12);
+            root.Right.Right.Right = new Node(15);
+            root.Right.Right.Left.Parent = root.Right.Right;
+            root.Right.Right.Right.Parent = root.Right.Right;
+
+            root.Right.Right.Left.Right = new Node(13);
+            root.Right.Right.Left.Right.Parent = root.Right.Right.Left;
+
+            Console.WriteLine("InOrder successor of 10 is");
+            Node result = GetInOrderSuccessor(root.Right);
+
+            if (result != null)
+            {
+                Console.WriteLine(result.Data);
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+
+            Console.WriteLine("InOrder successor of 13 is");
+            result = GetInOrderSuccessor(root.Right.Right.Left.Right);
+
+            if (result != null)
+            {
+                Console.WriteLine(result.Data);
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+
+            Console.WriteLine("InOrder successor of 15 is");
+            result = GetInOrderSuccessor(root.Right.Right.Right);
+
+            if (result != null)
+            {
+                Console.WriteLine(result.Data);
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+
         }
 
-        Node GetInOrderSuccessor(Node n)
+        static Node GetInOrderSuccessor(Node n)
         {
             if (n == null)
             {
@@ -58,32 +114,20 @@ namespace InOrderSuccessor
                 return LeftMostChild(n.Right);
             }
 
-            if (n.Parent != null)
+            while (n != null && n.Parent != null && n.Data >= n.Parent.Data)
             {
-                // n is left child so next node will be parent
-                if (n.Data < n.Parent.Data)
-                {
-                    return n.Parent;
-                }
-                else
-                {
-                    // n is right child but with no right subtree
-                    // so go up one level and get the parent's parent
-                    // For example, in above diagram for 13, the inorder
-                    // successor is 14 which is 2 levels up.
-                    while (n.Data >= n.Parent.Data)
-                    {
-                        n = n.Parent;
-                    }
-
-                    return n.Parent;
-                }
+                n = n.Parent;
             }
 
-            return null;
+            if (n == null)
+            {
+                return null;
+            }
+
+            return n.Parent;
         }
 
-        Node LeftMostChild(Node n)
+        static Node LeftMostChild(Node n)
         {
             while (n.Left != null)
             {
@@ -96,7 +140,12 @@ namespace InOrderSuccessor
 
     class Node
     {
-        public int Data { get; set; }
+        public Node(int data)
+        {
+            Data = data;
+        }
+
+        public int Data { get; private set; }
 
         public Node Left { get; set; }
 
